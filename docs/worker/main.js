5c881a2e83d4a6f6d9f9c790551554c4f26649a1
Irakli Gozalishvili <rfobic@gmail.com>
2019-01-15 19:35:38 -0800

    Update main.js

--- docs/worker/main.js
+++ docs/worker/main.js
@@ -2,7 +2,7 @@ const main = async () => {
   document.body.innerHTML += `\nActivating dedicated worker`
   const worker = new Worker(`./worker/worker.js?_=${Math.random().toString(36).slice(2)}`)
   document.body.innerHTML += "\nWaiting message from dedicated worker"
-  worker.onmessage = ({data}) => document.body.innerHTML += `\nSharedWorker: ${data}`
+  worker.onmessage = ({data}) => document.body.innerHTML += `\nWorker: ${data}`
   document.onclick = (event) => { if (event.target.id === "go") { workerFetch(worker) } }
   workerFetch(worker)
 }

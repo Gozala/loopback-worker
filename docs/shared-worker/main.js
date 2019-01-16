8dbb76520cd782b8b5afe9ff5af62a7c1c2a8eea
Irakli Gozalishvili <rfobic@gmail.com>
2019-01-15 18:50:56 -0800

    Update main.js

--- docs/shared-worker/main.js
+++ docs/shared-worker/main.js
@@ -1,14 +1,15 @@
 const main = async () => {
-  const url = new URL(location.href).searchParams.get("url") || "http://127.0.0.1:8080"
   document.body.textContent += `\nActivating shared worker that will try to fetch ${url} (you can customize url as query param)`
-  
   const worker = new SharedWorker(`./shared-worker/worker.js?url=${url}`)
-  worker.port.onmessage = (message) => {
-    document.body.textContent += `\n${message.data}`
-  }
   worker.port.start()
-
   document.body.textContent += "\nWaiting message from shared worker"
+  document.querySelector("button").onclick = () => workerFetch(worker)
+  const input = document.querySelector("input")
+  workerFetch(worker, input.value)
+}
+
+const workerFetch = (worker, url) => {
+  worker.postMessage({ fetch: url })
 }
 
 main()

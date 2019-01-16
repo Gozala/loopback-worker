a614942709c8b29a07ff798fe2cf3fc0df307cdb
Irakli Gozalishvili <rfobic@gmail.com>
2019-01-15 19:05:08 -0800

    Update main.js

--- docs/shared-worker/main.js
+++ docs/shared-worker/main.js
@@ -1,6 +1,6 @@
 const main = async () => {
   document.body.innerHTML += `\nActivating shared worker`
-  const worker = new SharedWorker(`./shared-worker/worker.js?${Math.random().toString(36).slice(2)`)
+  const worker = new SharedWorker(`./shared-worker/worker.js?_=${Math.random().toString(36).slice(2)}`)
   worker.port.start()
   document.body.innerHTML += "\nWaiting message from shared worker"
   document.querySelector("button").onclick = () => workerFetch(worker)

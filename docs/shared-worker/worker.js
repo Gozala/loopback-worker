b010b53c9b93dda68b1f74474643e33538e41644
Irakli Gozalishvili <rfobic@gmail.com>
2019-01-15 17:17:24 -0800

    Update worker.js

--- docs/shared-worker/worker.js
+++ docs/shared-worker/worker.js
@@ -1,16 +1,15 @@
 const main = async ({ports}) => {
-  const channel = ports[0];
-  channel.start()
-  const port = new URL(location.href).searchParams.get("port") || "8080"
-  const url = `http://127.0.0.1:${port}/`
-  channel.postMessage(`SharedWorker is fetching from ${url}`)
+  const [port] = ports;
+  port.start()
+  const url = new URL(location.href).searchParams.get("url") || "http://127.0.0.1:8080"
+  port.postMessage(`SharedWorker is fetching from ${url}`)
   try {
     const response = await fetch(url)
-    channel.postMessage(`SharedWorker got resoponse ${response.status}`)
+    port.postMessage(`SharedWorker got resoponse ${response.status}`)
     const text = await response.text()
-    channel.postMessage(`SharedWorker got data ${text}`)
+    port.postMessage(`SharedWorker got data ${text}`)
   } catch(error) {
-    channel.postMessage(`SharedWorker failet to fetch ${error.toString()}`)
+    port.postMessage(`SharedWorker failet to fetch ${error.toString()}`)
   }
 }
 

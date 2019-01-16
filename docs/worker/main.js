e9c7ea559ddf9ff502993ccac8e2457c36148f5b
Irakli Gozalishvili <rfobic@gmail.com>
2019-01-15 16:50:20 -0800

    Update main.js

--- docs/worker/main.js
+++ docs/worker/main.js
@@ -1,13 +1,13 @@
 const main = async () => {
   const port = new URL(location.href).searchParams.pors || 8080
-  document.body.textContent += `Activating dedicated worker that will try to fetch https://127.0.0.1:${port} (you can customize port as query param)`
+  document.body.textContent += `\nActivating dedicated worker that will try to fetch https://127.0.0.1:${port} (you can customize port as query param)`
   
   const worker = new Worker(`./worker/worker.js?port=${port}`)
   worker.onmessage = (message) => {
-    document.body.textContent += message.data
+    document.body.textContent += `\n${message.data}`
   }
 
-  document.body.textContent += "Waiting message from worker"
+  document.body.textContent += "\nWaiting message from worker"
 }
 
 main()

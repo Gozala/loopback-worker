c409922f4dce944c9375cf7b73e5eeaaf366c55b
Irakli Gozalishvili <rfobic@gmail.com>
2019-01-15 16:53:40 -0800

    Create main.js

---
+++ docs/shared-worker/main.js
@@ -0,0 +1,13 @@
+const main = async () => {
+  const port = new URL(location.href).searchParams.pors || 8080
+  document.body.textContent += `\nActivating shared worker that will try to fetch https://127.0.0.1:${port} (you can customize port as query param)`
+  
+  const worker = new SharedWorker(`./shared-worker/worker.js?port=${port}`)
+  worker.onmessage = (message) => {
+    document.body.textContent += `\n${message.data}`
+  }
+
+  document.body.textContent += "\nWaiting message from shared worker"
+}
+
+main()

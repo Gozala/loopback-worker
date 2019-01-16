ed397d525ace74f1bd71b80f2871e16d4e5774c1
Irakli Gozalishvili <rfobic@gmail.com>
2019-01-15 19:39:08 -0800

    Update worker.js

--- docs/worker/worker.js
+++ docs/worker/worker.js
@@ -1,18 +1,18 @@
 const main = async ({ports}) => {
   const [port] = ports;
-  port.postMessage(`SharedWorker client is connected`)
+  port.postMessage(`active`)
   port.onmessage = ({data}) => workerFetch(port, data.fetch)
 }
 
 const workerFetch = async (port, url) => {
   try {
-    port.postMessage(`SharedWorker is fetching ${url}`)
+    port.postMessage(`fetching ${url}`)
     const response = await fetch(url)
-    port.postMessage(`SharedWorker got resoponse ${response.status}`)
+    port.postMessage(`got resoponse ${response.status}`)
     const text = await response.text()
-    port.postMessage(`SharedWorker got data ${text}`)
+    port.postMessage(`got data ${text}`)
   } catch(error) {
-    port.postMessage(`SharedWorker failet to fetch ${error.toString()}`)
+    port.postMessage(`failet to fetch ${error.toString()}`)
   }
 }
 

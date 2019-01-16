ace36950d65a64e6646963b2bae746546f40a211
Irakli Gozalishvili <rfobic@gmail.com>
2019-01-15 16:50:42 -0800

    Update worker.js

--- docs/worker/worker.js
+++ docs/worker/worker.js
@@ -1,7 +1,7 @@
 const main = async () => {
   const port = new URL(location.href).searchParams.port || 8080
   const url = `http://127.0.0.1:${port}/`
-  self.postMessage("Worker is fetching from ${url}")
+  self.postMessage(`Worker is fetching from ${url}`)
   try {
     const response = await fetch(url)
     self.postMessage(`Worker got resoponse ${response.status}`)

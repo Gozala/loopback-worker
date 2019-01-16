1151b149d00da5eeff906bfcaf7b2296251c4050
Irakli Gozalishvili <rfobic@gmail.com>
2019-01-15 18:57:49 -0800

    Update main.js

--- docs/shared-worker/main.js
+++ docs/shared-worker/main.js
@@ -1,8 +1,8 @@
 const main = async () => {
-  document.body.textContent += `\nActivating shared worker`
+  document.body.innerHTML += `\nActivating shared worker`
   const worker = new SharedWorker(`./shared-worker/worker.js`)
   worker.port.start()
-  document.body.textContent += "\nWaiting message from shared worker"
+  document.body.innerHTML += "\nWaiting message from shared worker"
   document.querySelector("button").onclick = () => workerFetch(worker)
   const input = document.querySelector("input")
   workerFetch(worker, input.value)
